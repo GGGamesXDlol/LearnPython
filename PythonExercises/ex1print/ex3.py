6a1318629999e4eb55e796328703788341604484
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-04 19:23:34 +0100

    Perfección

--- PythonExercises/ex1print/ex3.py
+++ PythonExercises/ex1print/ex3.py
@@ -2,5 +2,5 @@ print("soy un cientifico matemático que estudia\n fisica cuantica, fisica, más
 print("*se acomoda las gafas*")
 print("entonces si divido esto y multiplico lo otro")
 print("2,23 por 89 entre 56 y luego pongo el 4,353 porciento da igual a:")
-print(2,23 * 89 / 56 % 4,353)
+print(2.23 * 89 / 56 % 4.353)
 print("perfección.")
