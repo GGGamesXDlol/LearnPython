3146c5a07a38b77d4e056b92a8d26ee5ee4654fe
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-04 19:50:46 +0100

    Viajes espaciales

--- PythonExercises/ex1print/ex4.py
+++ PythonExercises/ex1print/ex4.py
@@ -9,6 +9,6 @@ Naves_ocupadas = Voladores_disponibles + Naves
 Ufos_ocupados = Conductores_disponibles + Ufos
 
 
-print("Hola gtrqjska (nombre de alienijena (mujer)) hay " Naves_ocupadas " creo que no nos alcanzan sitios quedan porque quedan " Gente_en_cola " personas esperando.")
+print("Hola gtrqjska (nombre de alienijena (mujer)) hay ", Naves_ocupadas, " creo que no nos alcanzan sitios quedan porque quedan ", Gente_en_cola, " personas esperando.")
 print("No pasa nada jejsfegre (nombre de alienijena (hombre)) iremos otro día de vijaje.")
 print("ok nos vemos en una semana gtrqjska")
