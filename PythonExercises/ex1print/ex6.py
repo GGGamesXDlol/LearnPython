2005905209851132d05f272cedd7bec99b4077ae
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-10 19:57:36 +0100

    El Restauranteeee Supreme!!

--- PythonExercises/ex1print/ex6.py
+++ PythonExercises/ex1print/ex6.py
@@ -5,8 +5,8 @@ Restaurante_elegido = "buffet"
 Número_de_restaurante_elegido = 10
 y = f"Ya me decidi voy a un {Restaurante_elegido} que es el tipo de restaurante número {Número_de_restaurante_elegido}"
 
-print(x".")
-print(y".")
+print(x, ".")
+print(y, ".")
 
 print(f'Por que dije en voz alta "{x}".')
 print(f'Tambien porque dije "{y}" ahora todo el mundo que me escucho va a querer ir a uno :v.')
