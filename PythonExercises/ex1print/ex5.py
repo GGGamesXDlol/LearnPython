fb1bd80d48d03fc782f96f4291cb718fbf8b3fa3
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-10 19:32:29 +0100

    Soy un prrrrrrro!

---
+++ PythonExercises/ex1print/ex5.py
@@ -0,0 +1,14 @@
+mis_juegos = 50
+juegos_xbox = mis_juegos / 2
+juegos_ordenador = 25
+horas_que_he_jugado_de_cada_juego_en_un_mes = 120
+minutos_jugados_al_día_de_cada_juego = horas_que_he_jugado_de_cada_juego_en_un_mes / 24
+
+print("Hola, una pregunta, ¿Cuantos juegos tienes?.")
+print(f"Eso es facil yo tengo {mis_juegos}.")
+print("¿¡Tantos!?")
+print(f"No son tantos solo juego {horas_que_he_jugado_de_cada_juego_en_un_mes} horas al mes de cada juego.")
+print("Ooooh que interesante.")
+print(f"Cada día juego {minutos_jugados_al_día_de_cada_juego} minutos con cada juego.")
+print(f"Tengo {juegos_xbox} juegos en la xbox y {juegos_ordenador} en el ordenador.")
+print("Ehh... Yo hablaba de juegos de mesa :v.")
