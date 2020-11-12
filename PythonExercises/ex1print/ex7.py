1dd4cb003be3d60ac550c284acccea9c971c8d73
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-12 19:00:57 +0100

    El burdigato Supremeeeeeeeeeeeeeeeeeeee

---
+++ PythonExercises/ex1print/ex7.py
@@ -0,0 +1,27 @@
+print("\n")
+print("SoY uN dIaMaNtE")
+print("y SoY dE cOlOr AzUl CoMo El {}.".format('CiElOoOoOoOo'))
+print("." * 10)
+
+end1 = "E"
+end2 = "l"
+end3 = "B"
+end4 = "u"
+end5 = "r"
+end6 = "d"
+end7 = "i"
+end8 = "g"
+end9 = "a"
+end10 = "t"
+end11 = "o"
+end12 = "S"
+end13 = "u"
+end14 = "p"
+end15 = "r"
+end16 = "e"
+end17 = "m"
+end18 = "o"
+
+print(end1 + end2, end=' ')
+print(end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11, end=' ')
+print(end12 + end13 + end14 + end15 + end16 + end17 + end18)
