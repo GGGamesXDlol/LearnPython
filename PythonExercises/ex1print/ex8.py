11e35c6d8ad37f7f8e7f20a0dd9173a8347109ce
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-12 19:44:58 +0100

    Jhon Cenaaaaa!

---
+++ PythonExercises/ex1print/ex8.py
@@ -0,0 +1,12 @@
+hola = "{} {} {} {}"
+
+print(hola.format(1, 2, 3, 4))
+print(hola.format("hytale", "hoytelo", "gomate dif", "geometry dash"))
+print(hola.format(True, False, False, True))
+print(hola.format(hola, hola, hola, hola))
+print(hola.format(
+    "SoY...",
+    "jHOOOON",
+    "Cenaaaaaaaaaaaa!!!!",
+    "TU TURU TUUUUUU."
+))
