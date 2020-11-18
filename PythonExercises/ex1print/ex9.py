7305aabc003649dbf67de65ae80770e607e53c55
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-18 18:06:21 +0100

    ‏‎‎

---
+++ PythonExercises/ex1print/ex9.py
@@ -0,0 +1,85 @@
+mascotas = "Un perico, un gato, un perro, un pez y un camaleón."
+nombres = "Piquito, Pelusa, Manchas, Pepito y Aaron."
+
+print("Mira estos son los animales que tengo de mascota:", mascotas)
+print("Y estos son sus nombres, por cierto estan en el orden que usé para mencionar su especie:", nombres)
+print("." * 50)
+print("""
+SoN mUcHaS cOmIlLaS,
+aSí PuEdO eScRiBiR CuAnTaS
+lÍnEaS
+qUiErA :v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Jhooon Cenaaaaaaaaaaaaaaaaaaaaaaa!
+""")
