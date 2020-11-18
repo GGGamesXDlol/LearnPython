54aca3899d386fe0d76e6c9deb2502b145b004e1
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-18 18:52:17 +0100

    mainca + Hytale

--- PythonExercises/ex1print/ex11.py
+++ PythonExercises/ex1print/ex11.py
@@ -2,7 +2,7 @@ print("¿Te gusta minecraft?", end=' ')
 Me_gusta = input()
 print("¿Eres bueno o malo jugando?", end=' ')
 Como_juego = input()
-print("Cuando salga Hytale, ¿lo jugaras?")
+print("Cuando salga Hytale, ¿lo jugaras?", end=' ')
 Jugaras_Hytale = input()
 
 print(f"""Entoces, {Me_gusta} te gusta minecraft malo, te consideras {Como_juego} jugando y {Jugaras_Hytale} jugaras Hytale cuando salga,
