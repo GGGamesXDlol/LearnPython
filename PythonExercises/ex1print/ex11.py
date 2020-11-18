6f5f9b8ffb2947ade005ab1bb7284dcf2353091a
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-18 18:45:46 +0100

    mainca

---
+++ PythonExercises/ex1print/ex11.py
@@ -0,0 +1,9 @@
+print("¿Te gusta minecraft?", end=' ')
+Me_gusta = input()
+print("¿Eres bueno o malo jugando?", end=' ')
+Como_juego = input()
+
+print(f"""Entoces, {Me_gusta} te gusta minecraft malo y te consideras {Como_juego} jugando,
+
+eso fue todo gracias.
+""")
