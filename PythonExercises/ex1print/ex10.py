ca5f9512cc84c71f1681638f67932eb9628fed2f
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-18 18:18:37 +0100

    ‏‎‎ ¡Eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeel! ¡Primo!

--- PythonExercises/ex1print/ex10.py
+++ PythonExercises/ex1print/ex10.py
@@ -1,4 +1,4 @@
 print("Maneras de poner ' y \" sin que se confunda.")
-print("Un poner \ detras de un \".")
-print("""dos poner tres \""".""")
+print("Uno poner un \ detras de un \".")
+print("""Y dos poner tres \""".""")
 print("Eso fue todo por hoy publico, os saluda ¡Eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeel! ¡Primo!")
