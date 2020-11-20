9f03625b2913f7719cff17c45505607f341c998e
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-20 20:25:26 +0100

    unos experimentos mios :)

---
+++ PythonExercises/experimentos/experimentos.py
@@ -0,0 +1,10 @@
+habla = '- '
+
+nombre = input(f"""Hola, primero de todo, ¿cuál es tu nombre?
+{habla}""")
+
+print(f"Hola {nombre}.")
+
+cual = input(f"Tengo un problema \n {habla}")
+problema = input(f"Cuanto es 1 + 1? \n {habla}")
+print(f"Gracias {nombre} puedo morir en paz XD")
