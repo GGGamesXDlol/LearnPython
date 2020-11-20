b83029df5ee97026d372889919adb00ab4d6a7a4
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-20 19:55:54 +0100

    comando largo

---
+++ PythonExercises/ex1print/ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = argv
+
+print("Tu estas usando para ver esto:", script)
+print("Pero tu dices:", first)
+print("Y a eso respondes:", second)
+print("Y por último:", third)
