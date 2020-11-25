b1655586f999ef24aec66bd1e206e089336ec5d7
GGGamesXDlol <ikalesfe@gmail.com>
2020-11-25 19:26:48 +0100

    La fiesta

---
+++ PythonExercises/ex1print/ex19.py
@@ -0,0 +1,8 @@
+def Hamburguesas_burritos_y_helados(hamburguesas, burritos, helados):
+    print(f"─Toma nota, tenemos {hamburguesas} hamburguesas.")
+    print("─Si.")
+    print(f"─Tenemos {burritos} burritos.")
+    print("─Aja.")
+    print(f"─Y tenemos {helados} helados para la fiesta.")
+
+Hamburguesas_burritos_y_helados(50, 20, 20 * 3)
